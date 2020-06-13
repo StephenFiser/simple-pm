3f3bc855f6007030a61973d8a2c1707116ff5e6a
Stephen Fiser <stephen@stephens-mbp.lan>
2020-06-13 16:55:29 -0500

    styling changes; refactoring

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,7 +9,7 @@ class Task < ApplicationRecord
     ['Complete', 'complete']
   ]
 
-  def badge_color
+  def color_class
     case status
     when 'not-started'
       'secondary'
@@ -17,6 +17,17 @@ class Task < ApplicationRecord
       'info'
     when 'complete'
       'success'
+    end
+  end
+
+  def readable_status
+    case status
+    when 'not-started'
+      'Not started'
+    when 'in-progress'
+      'In progress'
+    when 'complete'
+      'Complete'
     end
   end
 
