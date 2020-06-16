492353aafd4d44f87f0d71996025de6d92427675
Stephen Fiser <stephen@stephens-mbp.lan>
2020-06-15 21:10:32 -0500

    wip

---
+++ app/javascript/controllers/tasks_controller.js
@@ -0,0 +1,12 @@
+import ApplicationController from './application_controller'
+
+/* This is the custom StimulusReflex controller for TasksReflex.
+ * Learn more at: https://docs.stimulusreflex.com
+ */
+export default class extends ApplicationController {
+
+  sort(event) {
+    console.log("here!")
+  }
+
+}
