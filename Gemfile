a256a258df43709c7ed87e31c19305176ec23277
Stephen Fiser <stephen@stephens-mbp.lan>
2020-07-21 15:14:08 -0500

    Adds ActionView::RecordIdentifier to Reflex

--- Gemfile
+++ Gemfile
@@ -59,4 +59,4 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem "stimulus_reflex", "~> 3.2"
+gem "stimulus_reflex", "3.3.0.pre2"
--- app/reflexes/status_reflex.rb
+++ app/reflexes/status_reflex.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class StatusReflex < ApplicationReflex
+  include ApplicationHelper
 
   def change
     task = Task.find(element.dataset[:id])
     task.update(status: element.dataset[:status])
+    morph dom_id(task), TasksController.render(TaskComponent.new(task: task))
   end
 
 end
